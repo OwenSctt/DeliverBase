87e3cd3ceac92363e769c3b8fc768edb698a3914
Owen Scott <oscott0202@gmail.com>
2025-05-12 23:12:49 -0400

    final draft

--- controllers/posts.js
+++ controllers/posts.js
@@ -26,14 +26,6 @@ module.exports = {
       console.log(err);
     }
   },
-  getFeed: async (req, res) => {
-    try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts, user: req.user });
-    } catch (err) {
-      console.log(err);
-    }
-  },
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
